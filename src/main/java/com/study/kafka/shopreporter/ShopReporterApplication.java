c499bda4872e31eb055ca65477781751f148ec44
eder.rodrigues <ederpr@gmail.com>
2022-06-09 06:29:38 -0300

    Commit inicial da aplicacao

---
+++ src/main/java/com/study/kafka/shopreporter/ShopReporterApplication.java
@@ -0,0 +1,13 @@
+package com.study.kafka.shopreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopReporterApplication.class, args);
+	}
+
+}
