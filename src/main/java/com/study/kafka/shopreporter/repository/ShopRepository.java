3b7dc185183774ff820bb164da0918ef9d1d29ad
eder.rodrigues <ederpr@gmail.com>
2022-06-09 06:56:53 -0300

    Interface com metodo que vai incrementar as vendas com seus respectivos status no banco de dados

--- src/main/java/com/study/kafka/shopreporter/repository/ShopRepository.java
+++ src/main/java/com/study/kafka/shopreporter/repository/ShopRepository.java
@@ -1,2 +1,14 @@
-package com.study.kafka.shopreporter.repository;public interface ShopRepository {
+package com.study.kafka.shopreporter.repository;
+
+import com.study.kafka.shopreporter.model.ShopReporter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShopRepository extends JpaRepository<ShopReporter, Long> {
+    @Modifying
+    @Query(value = "update shop_report set amount = amount+1 where identifier = :shopStatus", nativeQuery = true)
+    void incrementShopStatus(String shopStatus);
 }
