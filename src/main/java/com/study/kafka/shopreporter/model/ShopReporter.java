4f979ab92a3a5181501241177d8bdf41010db7df
eder.rodrigues <ederpr@gmail.com>
2022-06-09 06:56:04 -0300

    Classe que contem o modelo que vai armazenar as informacoes de relatorio

--- src/main/java/com/study/kafka/shopreporter/model/ShopReporter.java
+++ src/main/java/com/study/kafka/shopreporter/model/ShopReporter.java
@@ -1,2 +1,20 @@
-package com.study.kafka.shopreporter.model;public class ShopReporter {
+package com.study.kafka.shopreporter.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity(name="shop_report")
+public class ShopReporter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String identifier;
+    private Integer amount;
 }
